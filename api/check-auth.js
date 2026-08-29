0bfa1eefa074bc7d12c110c675d7e38b2145e40b
gastroosinfo-bit <gastro.os.info@gmail.com>
2026-08-29 06:32:21 +0200

    Update check-auth.js

--- api/check-auth.js
+++ api/check-auth.js
@@ -15,7 +15,8 @@ const DEMO_ACCESS = {
   'westerwinter@dehoga-nrw.de': { expires: '2026-08-15' },
   'franz.perner@wkbgld.at': { expires: '2026-08-15' },
   'z.asel@dehogabw.de': { expires: '2026-09-04' },
-  'natascha.kummer@wkbgld.at': { expires: '2026-09-02' }
+  'natascha.kummer@wkbgld.at': { expires: '2026-09-02' },
+  'info@gastronomen.koeln': { expires: '2026-09-15' }
 };
 
 function getDemoAccess(email) {
