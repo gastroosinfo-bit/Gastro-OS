8443441b5e1bd3099d807b57e23cefecbfed74dc
gastroosinfo-bit <gastro.os.info@gmail.com>
2026-09-19 09:50:21 +0200

    Update buch-public.js

--- api/buch-public.js
+++ api/buch-public.js
@@ -232,8 +232,7 @@ export default async function handler(req, res) {
 
     return res.status(200).json({
       items, meinName: name, kannBearbeiten: darfBearbeiten,
-      mitarbeiterNamen: darfBearbeiten ? mitarbeiterListe.map(m => m.name) : undefined,
-      _debugRechte: person.rechte
+      mitarbeiterNamen: darfBearbeiten ? mitarbeiterListe.map(m => m.name) : undefined
     });
   }
 
